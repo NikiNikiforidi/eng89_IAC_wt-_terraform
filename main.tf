0c883c1abea55edbb1b58cc86a25e9aaf38f3f81
NikiNikiforidi <86292184+NikiNikiforidi@users.noreply.github.com>
2021-08-17 13:34:31 +0300

    adding to main and varbale

--- main.tf
+++ main.tf
@@ -1,72 +1,96 @@
 # Let's build a script to connect to AWS and download/setup all dependencies required 
 
-
-
-# Setting up the SSH keys
-#resource "aws_key_pair" "deployer" {
-#  key_name = "eng89_devops"
-
-#  public_key = file("~/.ssh/eng89_devops.pem")
-#}
-
-
-
-
-
 # Keyword: provider aws
- provider "aws" {
-
-        region = "eu-west-1"
-
-}
-
-
-# then we will run: terraform init
 
 # then will move into launch aws services
 # Let's launch an ec2 instance in eu-west-1 with ami-038d7b856fe7557b3
 
 # keyword called "resource" provide resource name and give name with specific details to the service
 
-
 # recourse aws_ec2_instance, name it as eng89_niki_terraform, ami, type of instanc, with or without ip, tags is the keyword to name the instance eng89_niki_terraform
 
-
-resource "aws_instance" "app_instance" {
-
-
-
+# STEPS:
 # provide valid ami id, this is default ubuntu
-ami = "ami-038d7b856fe7557b3"
-
-
 # type of instance
-instance_type = "t2.micro"
-
-
 # we would like public ip for this instance
+# provide name to this instance using tags
+# ssh into instancwe
+# provide name to this instance using tags
+
+
+# -----------------------------------------------------------
+
+# Provide a region
+ provider "aws" {
+        region = "eu-west-1"
+}
+
+
+
+
+# Create a VPC  
+resource "aws_vpc" "terraform_vpc_code_test" {
+  cidr_block       = var.cidr_block_0 
+  instance_tenancy = "default"
+  
+  tags = {
+    Name = var.vpc_name
+  }
+} 
+
+
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "terraform_igw" {
+  vpc_id = aws_vpc.terraform_vpc_code_test.id
+  
+  tags = {
+    Name = var.igw_name
+  }
+}
+
+
+
+
+
+# DIORTHOSI PUBLIC SUBNET
+resource "aws_subnet" "terraform_subnet_public" {
+    vpc_id = aws_vpc.terraform_vpc_code_test.id
+    cidr_block = var.cidr_block_1
+    map_public_ip_on_launch = "true" 
+    availability_zone = "eu-west-1a"
+    tags = {
+        Name = var.pub_subnet_name
+    }
+}
+
+
+
+
+
+
+
+
+# Create Custom Route Table
+
+
+# Launch an instance
+resource "aws_instance" "app_instance" {
+ami                         = var.app_ami_id
+instance_type               = "t2.micro"
 associate_public_ip_address = true
 
-
-#key_name = "eng89_niki" # This is the same as below
-
-
-# provide name to this instance using tags
 tags = {
-      Name = "eng89_niki_terraform_app"       
+      Name = var.name      
 
  }
-
-
-
- key_name = var.aws_key_name # goes to varaible.tf
-
-
+ key_name = var.aws_key_name # goes to varaible.tf file
 
 }
 
 
 
+# ---------------------------------------------------------
 # most commonly used commands for terraform
 # terraform plan checks the syntax and validates the installations we have provided in this script
 
--- variable.tf
+++ variable.tf
@@ -1,11 +1,53 @@
 # let's create variable fo rour resources in mian.tf to make use of DRY
-variable "aws_key_name" {
-	default = "eng89_niki"
 
+
+variable "cidr_block_0" {
+  default="10.201.0.0/16"
 }
-variable "aws_key_path" {
 
-	default = "~/.ssh" 
-	# "~/.ssh/eng89_devops.pem"
 
+variable "cidr_block_1" {
+  default="10.201.1.0/16"
+}
+
+
+variable "vpc_name" {
+  default = "eng89_niki_terra_vpc"
+}
+
+
+
+variable "pub_subnet_name" {
+  default = "eng89_niki_terra_subnet_public"
+}
+
+
+variable "vpc_id" {
+
+  default = "vpc-07e47e9d90d2076da"
+}
+
+
+
+variable "igw_name" {
+  default = "eng89_niki_terra_IG"
+}
+
+
+
+variable "app_ami_id" {
+  default="ami-038d7b856fe7557b3"
+}
+
+
+# Let's creatge a variable to apply DRY
+
+variable "name" {
+  default="eng89_niki_terra_app"
+}
+
+
+
+variable "aws_key_name" {
+  default = "eng89_niki"
 }
